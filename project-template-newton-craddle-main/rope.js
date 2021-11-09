e6ee90b21a8dec9e11a2267dca52c3aef49da120
yellow-hello <89947416+yellow-hello@users.noreply.github.com>
2021-11-08 19:08:34 -0500

    Add files via upload

---
+++ project-template-newton-craddle-main/rope.js
@@ -0,0 +1,30 @@
+class rope{
+	constructor(body1,body2, offsetX)
+	{
+		var options={
+			bodyA:body1,
+			bodyB:body2
+		}
+
+		this.rope=Constraint.create(options)
+		World.add(world,this.rope)
+
+		this.offsetX = offsetX
+	}
+
+	display()
+	{
+
+		var pointA=this.rope.bodyA.position;
+		var pointB=this.rope.bodyB.position;
+
+		strokeWeight(2);
+
+		line(pointA.x, pointA.y, pointB.x + this.offsetX, pointB.y);
+
+		//WRITE THE CORRECT CODE TO RENDER A LINE BETWEEN THE TWO BODIES
+
+		
+	}
+
+}
